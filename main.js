368cffe173c5964b681e5ea1123e403c6b7bb17e
scratchis <103792439+scratchis@users.noreply.github.com>
2023-05-30 20:14:23 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+function setup(){
+    canvas= createCanvas(300,300);
+    canvas.center();
+    canvas.border(30);
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save("myFilterImage.png")
+}
