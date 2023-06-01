d7600df73def412f18e742a1d43f555e78b84826
scratchis <103792439+scratchis@users.noreply.github.com>
2023-06-01 19:37:47 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,11 +4,25 @@ function preload(){
 function setup(){
     canvas= createCanvas(300,300);
     canvas.center();
-    canvas.border(30);
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modalLoaded);
+    poseNet.on("pose",gotPoses);
+}
+function modalLoaded(){
+    console.log("PoseNet Is Initialized");
+}
+function gotPoses(result){
+    if(result.length > 0){
+        console.log(results);
+        console.log("nose x = "+results[0].pose.nose.x)
+        console.log("nose y = "+results[0].pose.nose.y)
+    }
 }
 function draw(){
-
+    image(video,0,0,300,300);
 }
 function take_snapshot(){
-    save("myFilterImage.png")
+    save("myFilterImage.png");
 }
